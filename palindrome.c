6fc12fc3af9663b7f6f76f8ca38ca298abd08950
ANUJASV <47479678+ANUJASV@users.noreply.github.com>
2019-02-20 16:08:25 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-int n,a;
+int n,a,b,r=0;
 void main()
 {
 	scanf("%d",&n);
+	b=n;
 	while(n!=0)
 	{
 	a=n%10;
-	printf("%d",a);
+	r=r*10+a;
 	n=n/10;
 	}
+	if(b==r)
+	printf("yes");
+	else
+	printf("no");
 }
